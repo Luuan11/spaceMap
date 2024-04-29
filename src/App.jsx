729f9046b0f4fb831264e811335dfa49b0dc17b4
Luuan11 <luancraft55@gmail.com>
2024-04-28 23:43:33 -0300

    :sparkles: feat: created the globalSttyles for the project

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import styled from "styled-components"
+import { styled } from "styled-components"
+import GlobalStyles from "./components/Globalstyles"
 
 const GradientBackround = styled.div`
   background: linear-gradient(174.61deg, #240433 4.16%, #27044f 48%, #4c1580 96.76%);
@@ -6,12 +7,11 @@ const GradientBackround = styled.div`
   min-height: 100vh;
 `
 
-
 function App() {
   
   return (
     <GradientBackround>
-      
+      <GlobalStyles />
     </GradientBackround>
   )
 }
