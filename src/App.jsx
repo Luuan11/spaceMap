2b6752e85d65629bc8a9354e9a1ea727c6b0169c
Luuan11 <luancraft55@gmail.com>
2024-05-02 22:12:06 -0300

    :sparkles: feat: rendering the Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { styled } from "styled-components"
 import GlobalStyles from "./components/Globalstyles"
+import Header from "./components/Header"
 
 const GradientBackround = styled.div`
   background: linear-gradient(174.61deg, #240433 4.16%, #27044f 48%, #4c1580 96.76%);
@@ -12,6 +13,7 @@ function App() {
   return (
     <GradientBackround>
       <GlobalStyles />
+      <Header />
     </GradientBackround>
   )
 }
