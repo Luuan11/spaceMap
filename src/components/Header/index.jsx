ef8bb67c1e41de011aacee86aad32e711123d11e
Luuan11 <luancraft55@gmail.com>
2024-05-02 22:14:23 -0300

    :sparkles: feat: importing the logo

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -4,12 +4,18 @@ const HeaderStyled = styled.header`
     padding: 60px 0;
     display: flex;
     justify-content: space-between;
+    
+    img {
+        max-width: 210px;
+        margin: 0 0 0 10px;
+
+    }
 `
 
 const Header = () =>{
     return (
     <HeaderStyled>
-            <img src="./assets/images/logo.png" alt="logo"/>
+            <img src="./public/images/logo.png" alt="logo"/>
     </HeaderStyled>
     )
 } 
