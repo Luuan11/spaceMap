b8bfb103ef5d8b93753e807772d0fdd0b46a40ea
Luuan11 <luancraft55@gmail.com>
2024-05-02 23:01:30 -0300

    :sparkles: feat: importing aside

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { styled } from "styled-components"
 import GlobalStyles from "./components/Globalstyles"
 import Header from "./components/Header"
+import AsideBar from "./components/AsideBar"
 
 const GradientBackround = styled.div`
   background: linear-gradient(174.61deg, #240433 4.16%, #27044f 48%, #4c1580 96.76%);
@@ -14,6 +15,7 @@ function App() {
     <GradientBackround>
       <GlobalStyles />
       <Header />
+      <AsideBar />
     </GradientBackround>
   )
 }
