8cbb607334f812acb2f5c6456678de9eaf8e2cd7
Luuan11 <luancraft55@gmail.com>
2024-05-30 19:07:47 -0300

    :bug: fix: changes on some files

--- src/components/Gallery/Famous/index.jsx
+++
@@ -1,6 +0,0 @@
-import Title from "../../Title"
-
-const Populares = () => {
-    return <Title>Populares</Title>
-}
-export default Populares
--- src/components/Title/index.tsx
+++
@@ -1,9 +0,0 @@
-import { styled } from "styled-components";
-
-const Title = styled.h2`
-    color: #7878e6;
-    font-size: 32px;
-    text-align: ${ props => props.$alinhamento ? props.$alinhamento : 'left' };
-`
-
-export default Title
